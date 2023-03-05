12594fb21deb61f780c7d6bbb4f276e37597a377
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-03-05 05:39:20 +0530

    final update

--- script.js
+++ script.js
@@ -10,11 +10,65 @@ submitButton.addEventListener('click', function() {
   messageInput.value = '';
 });
 
+var mentality_factor = 0;
+var validity = 1;
 function generateBotResponse(userMessage) {
     if(userMessage.toLowerCase() == 'hello')
-        return 'Hello!';
+        return 'Hello!. Can I ask some questions? (y/n)';
+    else if(userMessage.toLowerCase() == 'how are you?')
+        return 'I am fine, thank you!. Can I ask some questions? (y/n)';
+    else if(userMessage.toLowerCase() == 'what is your name?')
+        return 'My name is Chatbot. Can I ask some questions? (y/n)';
+    else if(userMessage.toLowerCase() == 'y'){
+        validity = 1;
+        return 'How are you feeling today? (good or bad)'; 
+    }
+    else if(userMessage.toLowerCase() == 'n'){
+        validity = 0;
+        return 'Okay. Have a nice day!'; 
+    } 
+    else if(userMessage.toLowerCase() == 'good' && validity == 1){
+        mentality_factor += 1;
+        return 'How many hours of sleep did you get last night? (1-10)';
+    }
+
+    else if(userMessage.toLowerCase() == 'bad' && validity == 1){
+        mentality_factor -= 1;
+        return 'How many hours of sleep did you get last night? (1-10)';
+    }
+
+    else if(parseInt(userMessage) > 5 && validity == 1){
+        mentality_factor += 1;
+        return 'Have you been feeling any stress or tension lately? (yes/no)';
+    }
+
+    else if(parseInt(userMessage) < 5 && validity == 1){
+        mentality_factor -= 1;
+        return 'Have you been feeling any stress or tension lately? (yes/no)';
+    }
+
+    else if(userMessage.toLowerCase() == 'no' && validity == 1){
+        mentality_factor += 1;
+        return 'Do you like to contact a counselor? (yes I want/no I dont want)';
+    }
     
-    return 'I am a chatbot! Nice to meet you.';
+    else if(userMessage.toLowerCase() == 'yes' && validity == 1){
+        mentality_factor -= 1;
+        return 'Do you like to contact a counselor? (yes I want/no I dont want)';
+    }
+
+    else if(userMessage.toLowerCase() == 'yes i want' && validity == 1){
+        return 'Okay. Pleace hold on..';
+    }
+
+    else if(userMessage.toLowerCase() == 'no i dont want' && validity == 1){
+        if(mentality_factor >= 2){
+            return 'Questionare is completed. You are in good mental health. Have a nice day!';
+        }
+        else{
+            return 'Questionare is completed. You are in bad mental health. Please contact a counselor. Have a nice day!';
+        }
+    }
 }
 
 function addMessageToChat(sender, message) {
